5a048af2126f1f2e1ae5aef1d1d97b736a02aaf2
Jong Shin <jshinm@gmail.com>
2026-08-07 07:35:12 +0600

    Fix: Run installdependencies.sh with proper sudo privileges

---
+++ current-datetime.js
@@ -0,0 +1,5 @@
+// Print current date and time
+const now = new Date();
+console.log('Current date and time:', now.toString());
+console.log('ISO format:', now.toISOString());
+console.log('Local format:', now.toLocaleString());
